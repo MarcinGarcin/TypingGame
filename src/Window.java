a1d9abf0ad2019711c190b1c853775d1810e4146
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-09 16:32:07 +0200

    First commit

---
+++ src/Window.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    public Window(){
+        this.setSize(800,600);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBackground(Color.black);
+    }
+}
