4ca29eb3baf1857cef81b75fdab3c4fa45b51e2e
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-10 16:38:11 +0200

    Initial commit

--- src/Window.java
+++ src/Window.java
@@ -2,10 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
+    int width = 1280;
+    int height = 720;
     public Window(){
-        this.setSize(800,600);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBackground(Color.black);
+        setPreferredSize(new Dimension(width, height));
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel startText = new JPanel();
+        startText.setSize(new Dimension(1280,720));
+        startText.setBackground(new Color(51,51,51));
+        add(startText);
+        pack();
+
+
     }
 }
