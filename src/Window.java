9fa30f5b7619ac7755bce28ca5efd374ffa9ca38
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-12 11:42:19 +0200

    Init commit

--- src/Window.java
+++ src/Window.java
@@ -10,12 +10,13 @@ public class Window extends JFrame {
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
 
         JPanel startText = new JPanel();
         startText.setSize(new Dimension(1280,720));
         startText.setBackground(new Color(51,51,51));
-        startText.setLayout(new GridLayout(3,3));
         JTextArea textArea = new JTextArea("Ssij pale");
+        textArea.
         startText.add(textArea);
         add(startText);
         pack();
