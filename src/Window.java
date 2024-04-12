79cc835c0c1d8a143cdeb5a72e9abcd4aacf5809
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-12 15:35:13 +0200

    Init commit

--- src/Window.java
+++ src/Window.java
@@ -1,26 +1,29 @@
 import javax.swing.*;
-import javax.swing.text.Position;
 import java.awt.*;
 
 public class Window extends JFrame {
     int width = 1280;
     int height = 720;
-    public Window(){
+
+    public Window() {
         setPreferredSize(new Dimension(width, height));
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        JPanel startText = new JPanel();
-        startText.setSize(new Dimension(1280,720));
-        startText.setBackground(new Color(51,51,51));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setSize(new Dimension(width, height));
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(new Color(51, 51, 51));
         JTextArea textArea = new JTextArea("Ssij pale");
-        textArea.
-        startText.add(textArea);
-        add(startText);
+
+        int textWidth = (width - textArea.getPreferredSize().width) / 2;
+        int textHeight = (height - textArea.getPreferredSize().height) / 2;
+        textArea.setBounds(textWidth, textHeight, textArea.getPreferredSize().width, textArea.getPreferredSize().height);
+        mainPanel.add(textArea);
+        add(mainPanel);
         pack();
-
-
     }
 }
+
