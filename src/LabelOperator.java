c5873b3b85b2dba417c4eb4938172e97f8c33c3e
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-15 23:38:28 +0200

    moving multiple labels to the right

--- src/LabelOperator.java
+++ src/LabelOperator.java
@@ -1,16 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class LabelOperator {
     private JPanel panel;
-    private Timer timer;
+    private ArrayList<Timer> timers;
     public ArrayList<JLabel> labels;
+    private String playerInput = "";
+    private Random rand;
 
-    public LabelOperator(JPanel panel ){
+    public LabelOperator(JPanel panel){
         this.panel = panel;
         setupTextArea();
         startGame();
@@ -18,22 +19,33 @@ public class LabelOperator {
 
     void startGame(){
         setupTextArea();
-        timer = new Timer(100, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JLabel currentLabel = labels.get(0);
-                currentLabel.setLocation(currentLabel.getX()+10, currentLabel.getY());
-                currentLabel.setText("Masny kurwa ben");
-                panel.repaint();
-                panel.revalidate();
-            }
-        });
-        timer.start();
-
+        timers = new ArrayList<Timer>();
+        for (int i = 0; i < 4; i++) {
+            final int finalIndex = i; // Dodajemy tę linię
+            keyTyper(labels.get(finalIndex));
+            Timer timer = new Timer(100, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JLabel currentLabel = labels.get(finalIndex);
+                    currentLabel.setLocation(currentLabel.getX()+10, currentLabel.getY());
+                    currentLabel.setText("chuj");
+                    panel.repaint();
+                    panel.revalidate();
+                }
+            });
+            timers.add(timer);
+            int delay = rand.nextInt(5000); // Losowe opóźnienie do 5 sekund
+            new Timer(delay, new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    timer.start();
+                }
+            }).start();
+        }
     }
+
     private void setupTextArea(){
         labels = new ArrayList<JLabel>();
-        Random rand = new Random();
+        rand = new Random();
         for(int i = 0;i<10;i++){
             JLabel label = new JLabel("label"+i);
             label.setBounds(-100,rand.nextInt(720),200,50);
@@ -43,7 +55,22 @@ public class LabelOperator {
             panel.add(label);
             labels.add(label);
         }
-
     }
-
+    private void keyTyper(JLabel label){
+        playerInput = "";
+        panel.setFocusable(true);
+        panel.requestFocusInWindow();
+        panel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                playerInput += e.getKeyChar();
+                if(label.getText().contains(playerInput)){
+                    label.setForeground(Color.GREEN);
+                }
+                else{
+                    label.setForeground(Color.RED);
+                }
+            }
+        });
+    }
 }
