efc205298d6462990e4a06538ce30f895d7bbf03
marcingarcin <knucias123@gmail.com>
2024-04-19 13:10:14 +0200

    Almost working but not as intended

--- src/LabelOperator.java
+++ src/LabelOperator.java
@@ -22,7 +22,7 @@ public class LabelOperator {
     void startGame() {
         setupTextArea();
         timers = new ArrayList<Timer>();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 10; i++) {
             final int finalIndex = i;
             JLabel currentLabel = labels.get(finalIndex);
             currentLabel.setText(texts[rand.nextInt(39)]);
@@ -30,7 +30,6 @@ public class LabelOperator {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     currentLabel.setLocation(currentLabel.getX() + 10, currentLabel.getY());
-                    System.out.println(currentLabel.getLocation());
                     panel.repaint();
                     panel.revalidate();
                 }
@@ -49,7 +48,7 @@ public class LabelOperator {
     private void setupTextArea() {
         labels = new ArrayList<JLabel>();
         rand = new Random();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 10; i++) {
             JLabel label = new JLabel("label" + i);
             label.setBounds(-200, rand.nextInt(680), 200, 50);
             label.setForeground(Color.white);
@@ -68,25 +67,26 @@ public class LabelOperator {
             private String playerInput = "";
             private String incorrectText = "";
             private int correctChars = 0;
+            JLabel currentLabel = labels.get(0);
+            String labelText = currentLabel.getText();
 
             @Override
             public void keyTyped(KeyEvent e) {
-                if (!labels.isEmpty()) {
-                    JLabel currentLabel = labels.get(0);
-                    String labelText = currentLabel.getText().replaceAll("<[^>]*>", "");
+                System.out.println(labelText);
+                System.out.println(playerInput);
+
+                char actualChar = e.getKeyChar();
+                if(!(actualChar == ' ')){
                     playerInput += e.getKeyChar();
-                    if (playerInput.equals(labelText.substring(0, playerInput.length()))) {
-                        correctChars = playerInput.length();
-                        updateLabelColor(currentLabel, correctChars,incorrectText);
-                        if (correctChars == labelText.length()) {
-                            currentLabel.setLocation(0, currentLabel.getY());
-                            System.out.println(currentLabel.getLocation());
-                            labels.remove(0);
-                            playerInput = "";
-                            correctChars = 0;
-                        }
-                    } else {
-                        playerInput = playerInput.substring(0, playerInput.length() - 1);
+                }
+                if (labelText.contains(playerInput)) {
+                    correctChars = playerInput.length();
+                    updateLabelColor(currentLabel, correctChars, incorrectText);
+                    if (correctChars == labelText.length()) {
+                        currentLabel.setLocation(0, currentLabel.getY());
+                        labels.remove(0);
+                        playerInput = "";
+                        correctChars = 0;
                     }
                 }
             }
@@ -97,6 +97,6 @@ public class LabelOperator {
         String text = label.getText();
         String correctText = text.substring(0, correctChars);
         String remainingText = text.substring(correctChars);
-        label.setText("<html><font color='green'>" + correctText +"</font><font color='red'>" + incorrectText + "</font></html>" + "</font><font color='white'>" + remainingText + "</font></html>");
+        label.setForeground(Color.green);
     }
 }
